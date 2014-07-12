e01fbddb31f25b2c05d9d232f346500d42b5b621
Dmitry Dmitrienko <dmitry.dmitrienko@outlook.com>
2014-07-12 15:05:42 +0400

    * Убрано дублирование кода из extension.js

--- extension.js
+++ extension.js
@@ -1,62 +1,41 @@
- // Copyright (c) 2012 The Codeant Authors. All rights reserved.
+// Copyright (c) 2012 The Codeant Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 /* Authors: Anton Sinitsyn <sinicin2008@gmail.com>
-            Dmitry Dmitrienko <dmitry.dmitrienko@outlook.com>
-            Nickolai Rashevsky <rashevsky.n@gmail.com>
-*/
+ Dmitry Dmitrienko <dmitry.dmitrienko@outlook.com>
+ Nickolai Rashevsky <rashevsky.n@gmail.com>
+ */
 
-chrome.downloads.onDeterminingFilename.addListener(function(item, __suggest) {
+chrome.downloads.onDeterminingFilename.addListener(function (item, __suggest) {
 
-  function TypeFile(name,attr,ext){
-    this._name = name;
-    this._attr = attr;
-    this._ext = ext;
-  };
-
-  var listSelect = {
-      "empty-type": new TypeFile("", "empty-type",[]),
-      "doc-type": new TypeFile("Документы", "doc-type",[
-      "docx",
-      "pdf",
-      "doc"
-    ]),
-    "torrent-type": new TypeFile("Torrents", "torrent-type",[
-      "torrent"
-    ]),
-    "pic-type": new TypeFile("Картинки", "pic-type",[
-      "jpeg",
-      "png",
-      "gif",
-    ])
-    };
-  
-function check_rule(rule, filename) {
-    var types = listSelect[rule.select_index]
-    for(var i = 0; i < types._ext.length; i++){
-      if (filename.indexOf(types._ext[i]) > -1) {
-        return true;
-      }
+    function check_rule(rule, profile, filename) {
+        var types = profile[rule.select_index];
+        for (var i = 0; i < types._ext.length; i++) {
+            if (filename.indexOf(types._ext[i]) > -1) {
+                return true;
+            }
+        }
+        return false;
     }
-	  return false;
-}
-function get_new_file_name(rules, filename) {
 
-    for (var i = 0; i < rules.length; i++) {
-      	if (check_rule(rules[i], filename)) {
-    		    return rules[i].path + "/" + filename;
-    	  }
+    function get_new_file_name(rules, profile, filename) {
+
+        for (var i = 0; i < rules.length; i++) {
+            if (check_rule(rules[i], profile, filename)) {
+                return rules[i].path + "/" + filename;
+            }
+        }
+        return filename;
     }
-    return filename;
-}
-  
-  chrome.storage.sync.get('DBE_data', function(items){
-  	  var rules = items.DBE_data;
-  	  var new_file_name = get_new_file_name(rules, item.filename);
-      __suggest({filename: new_file_name,
-           conflict_action: 'overwrite',
-           conflictAction: 'overwrite'});
-  });
-  
-  return true;
+
+    chrome.storage.sync.get('DBE_data', function (items) {
+        var data = items.DBE_data.data;
+        var profile = items.DBE_data.profile;
+        var new_file_name = get_new_file_name(data, profile, item.filename);
+        __suggest({filename: new_file_name,
+            conflict_action: 'overwrite',
+            conflictAction: 'overwrite'});
+    });
+
+    return true;
 });
--- options.js
+++ options.js
@@ -1,19 +1,19 @@
-function TypeFile(name,attr,ext){
-	this._name = name;
-	this._attr = attr;
-	this._ext = ext;
+function TypeFile(name, attr, ext) {
+    this._name = name;
+    this._attr = attr;
+    this._ext = ext;
 
-};
+}
 function Event(sender) {
     this._sender = sender;
     this._listeners = [];
 }
 
 Event.prototype = {
-    attach : function (listener) {
+    attach: function (listener) {
         this._listeners.push(listener);
     },
-    notify : function (args) {
+    notify: function (args) {
         var index;
 
         for (index = 0; index < this._listeners.length; index += 1) {
@@ -26,20 +26,20 @@ function ListModel(items) {
     this._items = items;
     this._selectedIndex = -1;
     this._listSelect = {
-    	"empty-type": new TypeFile("", "empty-type",[]),
-	    "doc-type": new TypeFile("Документы", "doc-type",[
-			"docx",
-			"pdf",
-			"doc"
-		]),
-		"torrent-type": new TypeFile("Torrents", "torrent-type",[
-			"torrent"
-		]),
-		"pic-type": new TypeFile("Картинки", "pic-type",[
-			"jpeg",
-			"png",
-			"gif",
-		])
+        "empty-type": new TypeFile("", "empty-type", []),
+        "doc-type": new TypeFile("Документы", "doc-type", [
+            "docx",
+            "pdf",
+            "doc"
+        ]),
+        "torrent-type": new TypeFile("Torrents", "torrent-type", [
+            "torrent"
+        ]),
+        "pic-type": new TypeFile("Картинки", "pic-type", [
+            "jpeg",
+            "png",
+            "gif",
+        ])
     };
 
     this.itemAdded = new Event(this);
@@ -48,37 +48,42 @@ function ListModel(items) {
 }
 
 ListModel.prototype = {
-    getItems : function () {
+    getItems: function () {
         return [].concat(this._items);
     },
 
-    addItem : function (item) {
+    addItem: function (item) {
         this._items.push({
-        	'select_index': item,
-        	'path': ''
+            'select_index': item,
+            'path': ''
         });
-        this.itemAdded.notify({ item : item });
+        this.itemAdded.notify({ item: item });
         this.save();
     },
 
-    removeItemAt : function (index) {
-    	this._items.splice(index, 1);
-        this.itemRemoved.notify();  
+    removeItemAt: function (index) {
+        this._items.splice(index, 1);
+        this.itemRemoved.notify();
         this.save();
     },
 
-    updateSelect: function(index, value){
-    	this._items[index].select_index = value;
+    updateSelect: function (index, value) {
+        this._items[index].select_index = value;
         this.save();
     },
 
-    updatePath: function(index, value){
+    updatePath: function (index, value) {
         this._items[index].path = value;
         this.save();
     },
 
-    save: function(){
-        chrome.storage.sync.set({'DBE_data': this._items}, function(){
+    save: function () {
+        chrome.storage.sync.set({
+            'DBE_data': {
+                'data': this._items,
+                'profile': this._listSelect
+            }
+        }, function () {
             console.log("saved");
         });
     }
@@ -103,23 +108,23 @@ function ListView(model, elements) {
     });
 
     this._elements.list.change(function (e) {
-        _this.listModified.notify({ index : e.target.selectedIndex });
+        _this.listModified.notify({ index: e.target.selectedIndex });
     });
     this._elements.addButton.click(function () {
         _this.addButtonClicked.notify();
     });
 
     $("#rules").on('click', this._elements.delButton, function () {
-    	var index = parseInt($(this).parents('tr').attr('id'));
+        var index = parseInt($(this).parents('tr').attr('id'));
         _this.delButtonClicked.notify(index);
     });
 
     $("#rules").on('change', this._elements.selectOptions, function () {
-    	var index = parseInt($(this).parents('tr').attr('id'));
-    	var new_value = $(this).children(":selected").attr("id");;
+        var index = parseInt($(this).parents('tr').attr('id'));
+        var new_value = $(this).children(":selected").attr("id");
         _this.selectOptionsChange.notify({
-        	'index': index,
-        	'value': new_value
+            'index': index,
+            'value': new_value
         });
     });
 
@@ -135,11 +140,11 @@ function ListView(model, elements) {
 }
 
 ListView.prototype = {
-    show : function () {
+    show: function () {
         this.rebuildList();
     },
 
-    rebuildList : function () {
+    rebuildList: function () {
         var list, items, key;
 
         list = this._elements.list;
@@ -150,26 +155,26 @@ ListView.prototype = {
         for (key in items) {
             if (items.hasOwnProperty(key)) {
                 var html = "<tr " + 'id="' + key + '">';
-				html += '<td>' + this.createHtmlSelect(items[key].select_index) + '</td>';
-				html += '<td>'+ '<input type="text" class="form-control input-dir" value="' + items[key].path +'"> </td>';
-				html += '<td>' + '<button type="button" class="del btn btn-default"><span class="glyphicon glyphicon-remove"></span></button>' +'</td>';
-				html += '</tr>';
-				list.prepend(html);
+                html += '<td>' + this.createHtmlSelect(items[key].select_index) + '</td>';
+                html += '<td>' + '<input type="text" class="form-control input-dir" value="' + items[key].path + '"> </td>';
+                html += '<td>' + '<button type="button" class="del btn btn-default"><span class="glyphicon glyphicon-remove"></span></button>' + '</td>';
+                html += '</tr>';
+                list.prepend(html);
             }
         }
     },
 
-    createHtmlSelect: function (typeSelect){
-		var html = '<select class="form-control type-file">';
-		for (key in this._model._listSelect){
-			html += '<option id="' + key + '"';
-			if (typeSelect === key)
-				html += ' selected="selected"';
-			html += ">" + this._model._listSelect[key]._name + '</option>';
-		}
-		html+='</select>';
-		return html;
-	}
+    createHtmlSelect: function (typeSelect) {
+        var html = '<select class="form-control type-file">';
+        for (key in this._model._listSelect) {
+            html += '<option id="' + key + '"';
+            if (typeSelect === key)
+                html += ' selected="selected"';
+            html += ">" + this._model._listSelect[key]._name + '</option>';
+        }
+        html += '</select>';
+        return html;
+    }
 };
 
 function ListController(model, view) {
@@ -186,61 +191,60 @@ function ListController(model, view) {
         _this.delItem(index);
     });
 
-    this._view.selectOptionsChange.attach(function(sender, data){
+    this._view.selectOptionsChange.attach(function (sender, data) {
         _this.updateSelect(data.index, data.value);
     });
 
-    this._view.pathChange.attach(function(sender, data){
+    this._view.pathChange.attach(function (sender, data) {
         _this.updatePath(data.index, data.value);
     });
 }
 
 ListController.prototype = {
-    addItem : function () {
+    addItem: function () {
         this._model.addItem("empty-type");
     },
 
-    delItem : function (index) {
+    delItem: function (index) {
 
         if (index !== -1) {
-        	var selector = "tr#"+index.toString();
-        	$(selector).remove();
+            var selector = "tr#" + index.toString();
+            $(selector).remove();
             this._model.removeItemAt(index);
         }
     },
 
-    updateSelect: function(index, value){
-    	if (index !== -1){
-    		this._model.updateSelect(index, value);
-    	}
+    updateSelect: function (index, value) {
+        if (index !== -1) {
+            this._model.updateSelect(index, value);
+        }
     },
 
-    updatePath: function(index, value){
+    updatePath: function (index, value) {
         if (index !== -1) {
             this._model.updatePath(index, value);
-        };
+        }
     }
 
 };
 
-$(document).ready(function(){
+$(document).ready(function () {
     var data = [];
+    //chrome.storage.sync.remove('DBE_data');
+    chrome.storage.sync.get('DBE_data', function (items) {
+        if ('DBE_data' in items) {
+            data = items.DBE_data;
+        }
+        var model = new ListModel(data);
+        view = new ListView(model, {
+            'list': $('#rules'),
+            'addButton': $('#add-rule'),
+            'delButton': ".del",
+            'selectOptions': '.type-file',
+            'pathInput': '.input-dir'
+        });
+        controller = new ListController(model, view);
 
-    chrome.storage.sync.get('DBE_data', function(items){
-            if ('DBE_data' in items){
-                data = items.DBE_data;
-            }
-            var model = new ListModel(data);
-            view = new ListView(model, {
-                'list' : $('#rules'), 
-                'addButton' : $('#add-rule'),
-                'delButton': ".del",
-                'selectOptions': '.type-file',
-                'pathInput': '.input-dir'
-            }),
-            controller = new ListController(model, view);
-            
-            view.show();
+        view.show();
     });
-  	//chrome.storage.sync.remove('DBE_data');
 });
